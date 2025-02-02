1487703d5eedb1288d1192de6343dd70d848b244
IdorosinerTau <117019553+IdorosinerTau@users.noreply.github.com>
2025-02-02 10:24:29 +0200

    Update create_db_script.py

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -18,22 +18,28 @@ def connect_to_database():
         exit(-1)
 
 
+# Movies(movie_id, title, overview, release_date, runtime, vote_average)
+# Fulltext - title, overview
+# Index - release_date, runtime, vote_average
+# Primary key - movie_id
 def create_Movies(connection):
-    # Movies(movie_id(primary key), title(full text index), overview, release_date,runtime,movie_rank(index), is_on_Netflix,is_on_AppleTV,is_on_AmazonPrime)
     cursor = connection.cursor()
-    query = """ CREATE TABLE Movies (
-                movie_id INT PRIMARY KEY, 
-                title varchar(500) NOT NULL, 
-                overview varchar(1500),
-                release_date DATE NOT NULL,
-                runtime smallint NOT NULL,
-                movie_popularity FLOAT NOT NULL,
-                vote_average FLOAT NOT NULL,
-                FULLTEXT(title),
-                FULLTEXT(overview),
-                INDEX(release_date),
-                INDEX(runtime)
-            ) """
+    query = """ 
+                CREATE TABLE Movies (
+                    movie_id INT PRIMARY KEY, 
+                    title varchar(500) NOT NULL, 
+                    overview varchar(1500),
+                    release_date DATE NOT NULL,
+                    runtime smallint NOT NULL,
+                    movie_popularity FLOAT NOT NULL,
+                    vote_average FLOAT NOT NULL,
+                    FULLTEXT(title),
+                    FULLTEXT(overview),
+                    INDEX(vote_average),
+                    INDEX(release_date),
+                    INDEX(runtime)
+                )
+            """
     try:
         cursor.execute(query)
         connection.commit()
@@ -42,16 +48,21 @@ def create_Movies(connection):
         connection.rollback()
 
 
+# MovieProviders(movie_id, provider_name)
+# Fulltext - provider_name
+# Primary key - (movie_id, provider_name)
+# Foreign key - movie_id references Movies.movie_id
 def create_MovieProviders(connection):
-    # Movies(movie_id(primary key), provider(full text index))
     cursor = connection.cursor()
-    query = """ CREATE TABLE MovieProviders (
-                movie_id INT, 
-                provider_name varchar(100), 
-                FULLTEXT(provider_name),
-                PRIMARY KEY (movie_id, provider_name),
-                foreign key(movie_id) references Movies(movie_id)
-            ) """
+    query = """ 
+                CREATE TABLE MovieProviders (
+                    movie_id INT, 
+                    provider_name VARCHAR(100), 
+                    FULLTEXT(provider_name),
+                    PRIMARY KEY(movie_id, provider_name),
+                    FOREIGN KEY(movie_id) REFERENCES Movies(movie_id)
+                ) 
+            """
     try:
         cursor.execute(query)
         connection.commit()
@@ -60,16 +71,18 @@ def create_MovieProviders(connection):
         connection.rollback()
     
 
+# MoviesFinance(movie_id, revenue)
+# Primary key - movie_id
+# Foreign key - movie_id references Movies.movie_id
 def create_MovieFinances(connection):
-    # MoviesFinance(movie_id, budget, revenue)
     cursor = connection.cursor()
-    query = """ CREATE TABLE MovieFinances (
-                movie_id INT PRIMARY KEY, 
-                budget BIGINT NOT NULL,
-                revenue BIGINT NOT NULL,
-                gross_profit BIGINT NOT NULL,
-                foreign key(movie_id) references Movies(movie_id)
-            ) """
+    query = """ 
+                CREATE TABLE MovieFinances (
+                    movie_id INT PRIMARY KEY, 
+                    revenue BIGINT NOT NULL,
+                    FOREIGN KEY(movie_id) REFERENCES Movies(movie_id)
+                ) 
+            """
     try:
         cursor.execute(query)
         connection.commit()
@@ -77,18 +90,41 @@ def create_MovieFinances(connection):
         print("Error creating MovieFinances table: " + str(e))
         connection.rollback()
 
-    
-def create_MovieGenres(connection):
-    # Genres(genre_id, genre_name)
+# Genres(genre_id, genre_name)
+# Fulltext - genre_name
+# Primary key - genre_id
+def create_Genres(connection):
     cursor = connection.cursor()
-    query = """ CREATE TABLE MovieGenres (
-                movie_id INT, 
-                genre_id INT,
-                genre_name varchar(50) NOT NULL,
-                PRIMARY KEY (movie_id, genre_id),
-                foreign key(movie_id) references Movies(movie_id),
-                FULLTEXT(genre_name)
-            ) """
+    query = """ 
+                CREATE TABLE Genres (
+                    genre_id INT PRIMARY KEY, 
+                    genre_name VARCHAR(50) NOT NULL,
+                    FULLTEXT(genre_name)
+                ) 
+            """
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print("Error creating MovieActors table: " + str(e))
+        connection.rollback()
+        
+        
+# MovieGenres(movie_id, genre_id)
+# Primary key - (movie_id, genre_id)
+# Foreign key - movie_id references Movies.movie_id
+#               genre_id references Genres.genre_id
+def create_MovieGenres(connection):
+    cursor = connection.cursor()
+    query = """ 
+                CREATE TABLE MovieGenres (
+                    movie_id INT, 
+                    genre_id INT,
+                    PRIMARY KEY(movie_id, genre_id),
+                    FOREIGN KEY(movie_id) REFERENCES Movies(movie_id),
+                    FOREIGN KEY(genre_id) REFERENCES Genres(genre_id)
+                ) 
+            """
     try:
         cursor.execute(query)
         connection.commit()
@@ -97,16 +133,20 @@ def create_MovieGenres(connection):
         connection.rollback()
 
 
+# Actors(actor_id, actor_name, gender, actor_popularity)
+# Fulltext - actor_name
+# Primary key - actor_id
 def create_Actors(connection):
-    # Actors(actor_id, actor_name(full text index), gender, actor_popularity)
     cursor = connection.cursor()
-    query = """ CREATE TABLE Actors (
-                actor_id INT PRIMARY KEY, 
-                actor_name varchar(100) NOT NULL, 
-                actor_gender enum('F','M') NOT NULL, 
-                actor_popularity FLOAT NOT NULL,
-                FULLTEXT(actor_name)
-            ) """
+    query = """ 
+                CREATE TABLE Actors (
+                    actor_id INT PRIMARY KEY, 
+                    actor_name VARCHAR(100) NOT NULL, 
+                    actor_gender ENUM('F','M') NOT NULL, 
+                    actor_popularity FLOAT NOT NULL,
+                    FULLTEXT(actor_name)
+                ) 
+            """
     try:
         cursor.execute(query)
         connection.commit()
@@ -114,17 +154,22 @@ def create_Actors(connection):
         print("Error creating Actors table: " + str(e))
         connection.rollback()
     
-    
+
+# MovieActor(movie_id, actor_id)
+# Primary key - (movie_id, actor_id)
+# Foreign key - movie_id references Movies.movie_id,
+#               actor_id references Actors.actor_id
 def create_MovieActors(connection):
-    # MovieActor(movie_id(primary key), genre_id(primary key))
     cursor = connection.cursor()
-    query = """ CREATE TABLE MovieActors (
-                movie_id INT, 
-                actor_id INT, 
-                PRIMARY KEY (movie_id, actor_id),
-                foreign key(movie_id) references Movies(movie_id),
-                foreign key(actor_id) references Actors(actor_id)
-            ) """
+    query = """ 
+                CREATE TABLE MovieActors (
+                    movie_id INT, 
+                    actor_id INT, 
+                    PRIMARY KEY(movie_id, actor_id),
+                    FOREIGN KEY(movie_id) REFERENCES Movies(movie_id),
+                    FOREIGN KEY(actor_id) REFERENCES Actors(actor_id)
+                ) 
+            """
     try:
         cursor.execute(query)
         connection.commit()
@@ -136,9 +181,11 @@ def create_MovieActors(connection):
 def create_all_tables():
     connection = connect_to_database()
     create_Movies(connection)
+    create_Genres(connection)
     create_MovieGenres(connection)
     create_MovieFinances(connection)
     create_Actors(connection)
     create_MovieActors(connection)
     create_MovieProviders(connection)
     connection.close()
+    print("Finished building all tables")
